b5b653b7da69fa3ed0c42defc38e32df06e3e82a
Luizzzin <luizhenribarbodias@gmail.com>
2023-08-07 22:50:14 -0300

    up
    up

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 function start() {
-
-    let cadastro=Number(prompt("INFORME AQUI O SEU NUMERO DE CADASTRO"));
+    let cadastro = Number(prompt("INFORME AQUI O SEU NUMERO DE CADASTRO"));
+    let resultadoElement = document.querySelector(".resultado");
     
+    if (cadastro >= 1000 && cadastro < 1999) {
+        document.querySelector(".body").style.backgroundColor = "var(--green)";
+        resultadoElement.textContent = "Acesso a todas áreas restritas";
+    } else {
+        resultadoElement.textContent = "Número de cadastro fora do intervalo.";
     }
+}
+
+
