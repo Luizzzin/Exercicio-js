8c25a2809d8b27f3596f5617e7b2387a8b5d1f64
Luizzzin <luizhenribarbodias@gmail.com>
2023-08-08 11:06:56 -0300

    up
    up

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ function start() {
     let cadastro = Number(prompt("INFORME AQUI O SEU NUMERO DE CADASTRO"));
     let resultado = document.querySelector(".resultado");
 
-    if (cadastro >= 1000 && cadastro < 1999) {
+    if (cadastro >= 1000 && cadastro <= 1999) {
         document.querySelector(".body").style.backgroundColor = "var(--green)";
         resultado.textContent = "Acesso a todas areas restritas";
         resultado.style.color = "var(--green)"
-
     }
-    else if (cadastro >= 2000 && cadastro < 2999) {
+
+    
+    else if (cadastro >= 2000 && cadastro <= 2999) {
         document.querySelector(".body").style.backgroundColor = "var(--yellow)";
         resultado.textContent = "Acesso limitado sobre algumas areas";
         resultado.style.color = "var(--yellow)"
     }
-    else 
-    {
+    else {
         document.querySelector(".body").style.backgroundColor = "var(--red)";
         resultado.textContent = "ACESSO NEGADO";
         resultado.style.color = "var(--red)"
